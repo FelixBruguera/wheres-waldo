913f863591d88e730b7f65f86594409cc511235f
Felix Bruguera <felixbruguera0@gmail.com>
2025-09-26 14:37:05 -0400

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,7 @@
+const App = () => {
+    return (
+        <h1>Helloooo</h1>
+    )
+}
+
+export default App
---
+++ vite.config.ts
@@ -0,0 +1,10 @@
+import { cloudflare } from '@cloudflare/vite-plugin'
+import { defineConfig } from 'vite'
+import react from "@vitejs/plugin-react"
+
+export default defineConfig({
+    server: {
+    port: 3001,
+  },
+  plugins: [cloudflare(),react()]
+})
