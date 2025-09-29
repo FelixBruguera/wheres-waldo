2912cde2f27ad8a866fe30a8ac7f215a8101c132
Felix Bruguera <felixbruguera0@gmail.com>
2025-09-29 16:44:06 -0400

    add min height to the leaderboard

--- src/components/Leaderboard.jsx
+++ src/components/Leaderboard.jsx
@@ -2,7 +2,7 @@ import { formatSeconds } from "../utils/formatSeconds"
 
 const Leaderboard = ({ data }) => {
     return (
-        <section className="w-8/10 lg:w-1/3 flex flex-col gap-3 px-2 py-3 rounded-lg bg-white/90 h-fit">
+        <section className="w-8/10 lg:w-1/3 flex flex-col gap-3 pt-2 rounded-lg bg-white/90 h-fit min-h-1/2">
             <h1 className="text-xl text-accent font-bold text-center">Leaderboard</h1>
             <table className="w-full">
                 <thead>
