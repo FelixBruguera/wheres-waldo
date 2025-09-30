8c3ee4ed573f711e4c80ab957fd684dd50a55b54
Felix Bruguera <felixbruguera0@gmail.com>
2025-09-30 06:38:49 -0400

    replace the instruction images with better ones

--- src/components/Instructions.jsx
+++ src/components/Instructions.jsx
@@ -8,13 +8,13 @@ const Instructions = ({ modal, found }) => {
                 <h3 className="text-xl col-span-1 col-start-2 font-bold">Find these characters</h3>
                 <Button className="ml-auto bg-accent text-white hover:bg-red-800" onClick={() => modal.current.close()}>Close</Button>
             </div>
-            <ul className="w-8/10 flex flex-wrap lg:flex-nowrap items-center justify-evenly bg-white mx-auto">
+            <ul className="w-9/10 flex flex-wrap lg:flex-nowrap items-center justify-evenly bg-white mx-auto">
                 { Object.entries(characters).map(([key, value]) => {
                     const isFound = found.some(char => char.name === key)
                     return (
-                    <li key={key} className="flex flex-col gap-1 lg:h-70">
+                    <li key={key} className="flex flex-col gap-1 lg:h-60">
+                        <img src={`/${value}`} alt={key} className={`size-30 lg:size-35 ${isFound && "opacity-50"}`} />
                         <p className="text-center">{key}</p>
-                        <img src={`/${value}`} alt={key} className={`w-30 h-55 lg:size-50 ${isFound && "opacity-50"}`} />
                         {isFound && <p className="text-center text-lg font-bold text-accent">Found</p>}
                     </li>)
                 })}
